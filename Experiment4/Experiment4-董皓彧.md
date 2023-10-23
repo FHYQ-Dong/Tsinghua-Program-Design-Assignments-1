7bfd02b4340afcd10c6dda26f8738a5aaf724920
FHYQ-Dong <FHYQ-Dong-Work@outlook.com>
2023-10-23 20:45:13 +0800

    Report-Generator

---
+++ Experiment4/Experiment4-董皓彧.md
@@ -0,0 +1,193 @@
+# Experiment4-董皓彧
+环境: 
+```txt
+gcc.exe (x86_64-win32-seh-rev0, Built by MinGW-W64 project) 8.1.0
+Visual Stdio Code 1.83.1
+```
+作业的github地址: 
+[https://github.com/FHYQ-Dong/Tsinghua-Program-Design-Assignments/tree/main/Experiment4](https://github.com/FHYQ-Dong/Tsinghua-Program-Design-Assignments/tree/main/Experiment4)
+## 必做题
+### compulsive/Experiment4-1
+代码: 
+```c
+#include<stdio.h>
+#include<math.h>
+#define pi acos(-1)
+
+void print_exp() {
+    printf("input x: ");
+    double x = 0;
+    scanf("%lf", &x);
+    double tmp = exp(-0.5 * x * x);
+    printf("f(x) = %lf\n", (1/sqrt(2 * pi)) * tmp);
+    return;
+}
+
+void printf_sin_cos() {
+    printf("input x y: ");
+    double x = 0, y = 0;
+    scanf("%lf %lf", &x, &y);
+    printf("x = %lf, y = %lf\n", x, y);
+    printf("f(x) = %lf\n", (double)(1)/3 * sin(x*x + y*y) * cos(x+y));
+    return;
+}
+
+int main() {
+    print_exp();
+    printf("\n");
+    printf_sin_cos();
+    return 0;
+}
+```
+输入1: 
+```txt
+
+```
+输出1: 
+```txt
+input x: f(x) = 0.398942
+
+input x y: x = 0.000000, y = 0.000000
+f(x) = 0.000000
+```
+
+### compulsive/Experiment4-2
+代码: 
+```c
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    printf("(!x && x!=0) == false\n");
+    printf("(!(x==a) && (y==b) && 0) == false\n");
+    printf("(-10<a<5 && b==c) == false\n");
+    printf("(5>3 && 2 || 8<4-!0) == true\n");
+    printf("(!4<y<5 && 5<b<6) == true\n");
+    printf("(!x || x!=0) == true\n");
+    printf("(3<x<5 || y>3 && y<2) == true\n");
+    return 0;
+}
+```
+输入1: 
+```txt
+
+```
+输出1: 
+```txt
+(!x && x!=0) == false
+(!(x==a) && (y==b) && 0) == false
+(-10<a<5 && b==c) == false
+(5>3 && 2 || 8<4-!0) == true
+(!4<y<5 && 5<b<6) == true
+(!x || x!=0) == true
+(3<x<5 || y>3 && y<2) == true
+```
+
+### compulsive/Experiment4-3
+代码: 
+```c
+#include<stdio.h>
+#include<stdlib.h>
+#define true 1
+#define false 0
+typedef int bool;
+
+bool a = false, b = false, c = false; // false: 不是泄密者
+
+bool argument_a(bool b, bool honest) { // b: a的谈论对象, honest: a说的话的真假
+    return honest ? b==1 : b==0;
+}
+bool argument_b(bool c, bool honest) { // c: b的谈论对象, honest: b说的话的真假
+    return honest ? c==1 : c==0;
+}
+bool argument_c(bool argv_b, bool honest) { // argv_b: b的谈论对象, honest: c说的话的真假
+    return honest ? argument_b(argv_b, false) : argument_b(argv_b, true);
+}
+
+void init(int x) {
+    a = x == 1;
+    b = x == 2;
+    c = x == 3;
+    return;
+}
+
+bool check() {
+    bool result = false;
+    for(int ha=0; ha<=1; ++ha) {
+        for(int hb=0; hb<=1; ++hb) {
+            for(int hc=0; hc<=1; ++hc) {
+                result |= argument_a(b, ha) && argument_b(c, hb) && argument_c(c, hc);
+                if (argument_a(b, ha) && argument_b(c, hb) && argument_c(c, hc)) {
+                    printf("若甲说%s话, 乙说%s话, 丙说%s话, 则:\n", ha?"真":"假", hb?"真":"假", hc?"真":"假");
+                }
+                
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+    bool flag = false;
+    for(int i=1; i<=3; ++i) {
+        init(i);
+        if(check()) printf("可能的泄密者是%s\n", i==1?"甲":i==2?"乙":"丙"), flag = true;
+    }
+    if(!flag) printf("没有泄密者\n");
+    return 0;
+}
+```
+输入1: 
+```txt
+
+```
+输出1: 
+```txt
+若甲说假话, 乙说假话, 丙说真话, 则:
+可能的泄密者是甲
+若甲说真话, 乙说假话, 丙说真话, 则:
+可能的泄密者是乙
+若甲说假话, 乙说真话, 丙说假话, 则:
+可能的泄密者是丙
+```
+
+## 选做题
+### optional/Optional-Experiment4-1
+代码: 
+```c
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int bool;
+#define true 1
+#define false 0
+
+int cnt15, cnt10, cnt5;
+bool flag;
+
+int main() {
+    for(cnt15=98; cnt15>0; --cnt15) {
+        for(cnt10=99-cnt15; cnt10>0; --cnt10) {
+            cnt5 = 100 - cnt15 - cnt10;
+            if(cnt15*15 + cnt10*10 + cnt5*5 == 1000) {
+                flag = true;
+                goto end;
+            }
+        }
+    }
+end:
+    if(flag) printf("%d只母鸡, %d只公鸡, %d只小鸡\n", cnt15, cnt10, cnt5);
+    else printf("无解\n");
+    return 0;
+}
+
+```
+输入1: 
+```txt
+
+```
+输出1: 
+```txt
+49只母鸡, 2只公鸡, 49只小鸡
+```
+
---
+++ utils/generate_report.py
@@ -0,0 +1,118 @@
+from sys import argv
+from loguru import logger
+import subprocess
+import os.path
+import json
+
+class MarkdownCreater:
+    def __init__(self, path) -> None:
+        self.path = path
+        if os.path.isfile(self.path):
+            logger.warning(f'File {self.path} already exists, overwrite it? [y/n]')
+            if input() != 'y':
+                logger.info('Abort.')
+                exit(0)
+            else:
+                logger.info('Overwrite.')
+
+    def create_markdown(self):
+        self.markdown = open(self.path, 'w', encoding='utf-8')
+    def close_markdown(self):
+        self.markdown.close()
+
+    def markdown_write_title(self, title):
+        self.markdown.write(f'# {title}\n')
+    def markdown_write_subtitle(self, subtitle):
+        self.markdown.write(f'## {subtitle}\n')
+    def markdown_write_subsubtitle(self, subsubtitle):
+        self.markdown.write(f'### {subsubtitle}\n')
+
+    def markdown_write_link(self, link, text):
+        self.markdown.write(f'[{text}]({link})')
+    def markdown_write_code(self, code, code_lang):
+        self.markdown.write(f'```{code_lang}\n{code}\n```\n')
+    def markdown_write_program(self, program, program_lang):
+        self.markdown.write(f'代码: \n')
+        self.markdown_write_code(program, program_lang)
+    def markdown_write_input(self, input, input_id):
+        self.markdown.write(f'输入{input_id}: \n```txt\n{input}\n```\n')
+    def markdown_write_output(self, output, output_id):
+        self.markdown.write(f'输出{output_id}: \n```txt\n{output}\n```\n')
+    def markdown_write_newline(self):
+        self.markdown.write('\n')
+
+class ProjectReportCreater():
+    def __init__(self, directory) -> None:
+        self.directory = directory
+        self.configFile = open(os.path.join(directory, 'Project.conf'), 'r', encoding='utf-8')
+        self.confargv = json.load(self.configFile)
+        self.configFile.close()
+
+        self.Name = self.confargv['Name']
+        self.Url = self.confargv['Url']
+        self.Environment = self.confargv['Environment']
+        self.Experiments = self.confargv['Experiments']
+
+        self.mdCreater = MarkdownCreater(os.path.join(directory, self.Name+'.md'))
+        self.mdCreater.create_markdown()
+
+    def get_one_experiment_result(self, Experiment:dict):
+        ExperimentResult = {}
+        with open(os.path.join(self.directory, Experiment['Name']+'.c'), 'r', encoding='utf-8') as f:
+            code = f.read()
+            ExperimentResult['Code'] = code
+        Inputs, Outputs = Experiment['Inputs'], []
+        for Input in Inputs:
+            Output = subprocess.check_output(os.path.join(self.directory, Experiment['Name']+'.exe'), input=Input.encode()).decode()
+            Output = Output.replace('\r\n', '\n')
+            if Output[-1] == '\n':
+                Output = Output[:-1]
+            Outputs.append(Output)
+
+        ExperimentResult['Input'] = Inputs
+        ExperimentResult['Output'] = Outputs
+        logger.info(f'{Experiment["Name"]} done.')
+        return ExperimentResult
+    
+    def write_report(self):
+        self.mdCreater.markdown_write_title(self.Name)
+        self.mdCreater.markdown.write('环境: \n')
+        self.mdCreater.markdown_write_code('\n'.join(self.Environment), 'txt')
+        self.mdCreater.markdown.write('作业的github地址: \n')
+        self.mdCreater.markdown_write_link(self.Url, self.Url)
+        self.mdCreater.markdown_write_newline()
+        self.mdCreater.markdown_write_subtitle('必做题')
+        for Experiment in self.Experiments['compulsive']:
+            self.mdCreater.markdown_write_subsubtitle(Experiment['Name'])
+            ExperimentResult = self.get_one_experiment_result(Experiment)
+            self.mdCreater.markdown_write_program(ExperimentResult['Code'], 'c')
+            for idx in range(len(ExperimentResult['Input'])):
+                self.mdCreater.markdown_write_input(ExperimentResult['Input'][idx], idx+1)
+                self.mdCreater.markdown_write_output(ExperimentResult['Output'][idx], idx+1)
+            self.mdCreater.markdown_write_newline()
+        if 'optional' in self.Experiments:
+            if len(self.Experiments['optional']) > 0:
+                self.mdCreater.markdown_write_subtitle('选做题')
+                for Experiment in self.Experiments['optional']:
+                    self.mdCreater.markdown_write_subsubtitle(Experiment['Name'])
+                    ExperimentResult = self.get_one_experiment_result(Experiment)
+                    self.mdCreater.markdown_write_program(ExperimentResult['Code'], 'c')
+                    for idx in range(len(ExperimentResult['Input'])):
+                        self.mdCreater.markdown_write_input(ExperimentResult['Input'][idx], idx+1)
+                        self.mdCreater.markdown_write_output(ExperimentResult['Output'][idx], idx+1)
+                    self.mdCreater.markdown_write_newline()
+        self.close_report()
+
+    def close_report(self):
+        logger.info('Report generated.')
+        self.mdCreater.close_markdown()
+
+if __name__ == '__main__':
+    if len(argv) == 1:
+        myReportCreater = ProjectReportCreater('.')
+    elif len(argv) == 2:
+        myReportCreater = ProjectReportCreater(argv[1])
+    else:
+        logger.error('Too many arguments.')
+        exit(1)
+    myReportCreater.write_report()
