78049f1a8454c69bee2e93c823b9228879cbdc50
FHYQ-Dong <FHYQ-Dong-Work@outlook.com>
2023-11-12 01:50:11 +0800

    SSP-Online-Server

--- Experiment7/SSP-Online/SSP-Online-Server/include/bool.h
+++ Experiment7/SSP-Online/SSP-Online-Server/include/bool.h
@@ -2,5 +2,5 @@
 #define _Bool_H
 #define true 1
 #define false 0
-typedef int bool;
+typedef char bool;
 #endif
--- Experiment7/SSP-Online/SSP-Online-Server/server.c
+++ Experiment7/SSP-Online/SSP-Online-Server/server.c
@@ -1,8 +1,12 @@
 #include "./include/player.h"
 #include "./include/judge.h"
 #include "./include/bool.h"
+#include <stdio.h>
 
 int main() {
     bool x;
+    Player *player1 = newPlayer("player1", 1);
+    scanf("%c", &x);
+    printf("%c\n", x);
     return 0;
 }
