4f24cbb71e743bd972879e80b63d59afc3f568d0
FHYQ-Dong <FHYQ-Dong-Work@outlook.com>
2023-12-09 18:45:15 +0800

    Experiment11

---
+++ Experiment11/compulsive/Experiment11-3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+typedef char bool;
+typedef int* intp;
+#define true 1
+#define false 0 
+
+void bsort(intp* begin, intp* end, bool (*cmp)(intp, intp)) {
+    if (begin >= end) return;
+    for (intp* i = begin; i < end; ++i) {
+        for (intp* j = i+1; j < end; ++j) {
+            if (cmp(*j, *i)) {
+                intp t = *i;
+                *i = *j;
+                *j = t;
+            }
+        }
+    }
+    return;
+}
+
+bool cmp(intp a, intp b) { return *a < *b; }
+
+int main() {
+    int a[10], *pa[10];
+    for (int i=0; i<10; ++i) scanf("%d", &a[i]), pa[i] = &a[i];
+    bsort(pa, pa+10, cmp);
+    for (int i=0; i<10; ++i) printf("%d ", *pa[i]);
+    printf("\n");
+    for (int i=0; i<10; ++i) printf("%d ", pa[i]-a);
+}
